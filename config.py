f007e86d99dba774fa29f82339ef1001d8560e3b
Andres <edwin.morales-s@uniminuto.edu.co>
2026-05-23 23:06:22 -0500

    se agrega micro de pedidos al config.py

--- config.py
+++ config.py
@@ -14,9 +14,8 @@ class Config:
         "productos": os.getenv("CATALOG_SERVICE_URL", "http://127.0.0.1:5002/api/productos"),
         
         # Sprint 4 5003
-        "inventario": os.getenv("INVENTORY_SERVICE_URL", "http://127.0.0.1:5003/api/inventario")
+        "inventario": os.getenv("INVENTORY_SERVICE_URL", "http://127.0.0.1:5003/api/inventario"),
         
-        # Futuros microservicios (Sprints 4 y 5):
-        # "inventario": "http://127.0.0.1:5003/api/inventario",
-        # "pedidos": "http://127.0.0.1:5004/api/pedidos"
+        # Sprint 5 y 6 5004
+        "pedidos": os.getenv("ORDERS_SERVICE_URL", "http://127.0.0.1:5004/api/pedidos")
     }
